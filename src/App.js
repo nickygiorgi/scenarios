41d6d33dd3991dc3148db69d955c1b0b30f40512
Nicole Giorgi <nicolegiorgi@gmail.com>
2020-09-11 15:15:36 +0100

    Wiring app react app with firebase project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import firebase from './firebase'
 
 function App() {
+
+  const [scenarios, setScenarios] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore();
+      const data = await db.collection("scenarios").get();
+      setScenarios(data.docs.map(doc => doc.data()));
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Scenarios here!</h1>
+       <ul>
+       {scenarios.map(s => (
+         <li key={s.id}>{s.title}</li>
+       ))}
+       </ul>
     </div>
   );
 }
